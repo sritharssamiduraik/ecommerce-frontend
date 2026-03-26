e1612257d18805962b250070606e07679ad9d1a7
sritharssamiduraik <sritharsamiduraik@gmail.com>
2026-03-26 10:29:15 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-222b.up.railway.app/api"
+  baseURL: "ecommerce-backend-production-49a9.up.railway.app/api"
 });
 
 export default API;
